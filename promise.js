157393c737068c8092e47774c05ea20819b7a8e5
Hendra Wijaya <wijaya.hndra@gmail.com>
2022-06-12 19:29:23 +0700

    Done Promise

--- promise.js
+++ promise.js
@@ -1,7 +1,19 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (emosi) => {
+  try{
+    const [surveyIXX, surveyVGC] = await Promise.all([
+      promiseTheaterIXX(),
+      promiseTheaterVGC()
+    ]);
+
+    const surveyResult = [...surveyIXX, ...surveyVGC];
+    return surveyResult?.filter((survey) => survey.hasil === emosi)?.length || 0;
+  }catch (err){
+    console.log('Error found, pls check ' + err);
+  }
+};
 
 module.exports = {
   promiseOutput,
